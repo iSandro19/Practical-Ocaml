d5d3be03827d737960fded5b73a665b5873b5c93
iSandro19 <manteigaseoaneoscar@gmail.com>
2020-11-20 16:35:58 +0100

    Definitivas P3, P4 y P5

--- P5/mylist2.ml
+++ P5/mylist2.ml
@@ -19,9 +19,10 @@ let tl l =
 
 (* Función length *)
 let rec length l =
-  match l with
-    [] -> 0
-  | _::t -> 1 + length t;;
+  let rec aux len = function
+    [] -> len
+  | _::t -> aux (len+1) t in
+  aux 0 l;;
 
 (* Función compare_lengths *)
 let rec compare_lengths l1 l2 =
@@ -38,14 +39,16 @@ let rec nth l p =
   else
     match l with
       [] -> raise (Failure "nth")
-    | h::t -> if p = 0 then h
-              else nth t (p-1);;  
+    | h::t ->
+      if p = 0
+        then h
+      else nth t (p-1);;  
 
 (* Función append *)
 let rec append l1 l2 =
   match l1 with
     [] -> l2
-  | h::t -> h::append t l2;;
+  | h::t -> h::append t l2;; (* No recursiva terminal! *)
 
 (* SEGUNDA PARTE *)
 
@@ -53,22 +56,28 @@ let rec append l1 l2 =
 let rec find c l=
   match l with
     [] -> raise (Not_found)
-  | h::t -> if (c h) then h
-            else find c t;;
+  | h::t ->
+    if (c h)
+      then h
+    else find c t;;
 
 (* Función for_all *)
 let rec for_all c l =
   match l with
     [] -> true
-  | h::t -> if c h then for_all c t
-            else false;;
+  | h::t ->
+    if c h
+      then for_all c t
+    else false;;
 
 (* Función exists *)
 let rec exists c l =
   match l with
     [] -> false
-  | h::t -> if c h then true
-            else exists c t;;
+  | h::t ->
+    if c h
+      then true
+    else exists c t;;
 
 (* Función mem *)
 let rec mem i l =
@@ -77,38 +86,64 @@ let rec mem i l =
   | h::t -> i = h || mem i t;;
 
 (* Función filter *)
-let rec filter c l =
-  match l with
-    [] -> []
-  | h::t -> if c h then h::filter c t 
-            else filter c t;;
+let rec filter c =
+  let rec aux laux = function
+  | [] ->
+    let rec rev aux = function
+      | [] -> aux
+      | h::t -> rev (h::aux) t in
+    rev [] laux
+  | h::t ->
+    if c h
+      then aux (h::laux) t
+    else
+      aux laux t in
+  aux [];;
 
 (* Función find_all *)
-let rec find_all c l =
-  match l with
-    [] -> []
-  | h::t -> if c h then h::find_all c t 
-            else find_all c t;;
+let find_all c =
+  let rec aux laux = function
+  | [] ->
+    let rec rev aux = function
+      | [] -> aux
+      | h::t -> rev (h::aux) t in
+    rev [] laux
+  | h::t ->
+    if c h
+      then aux (h::laux) t
+    else
+      aux laux t in
+  aux [];;
 
 (* Función partition *)
 let rec partition c l =
-  match l with
-    [] -> ([],[])
-  | h::t -> if c h then (h::fst(partition c t), snd(partition c t))  
-            else (fst(partition c t), h::snd(partition c t));;
+  let rec aux cumple no_cumple = function
+    | [] ->
+      let rec rev aux = function
+        | [] -> aux
+        | h::t -> rev (h::aux) t in
+      (rev [] cumple, rev [] no_cumple)
+    | h::t ->
+      if c h
+        then aux (h::cumple) no_cumple t
+      else
+        aux cumple (h::no_cumple) t in
+    aux [] [] l;;
 
 (* Función split *)
 let rec split l =
   match l with
     [] -> ([],[])
-  | h::t -> (fst h::fst(split t), snd h::snd(split t));;
+  | h::t ->
+    (fst h::fst(split t), snd h::snd(split t));; (* No recursiva terminal! *)
 
 (* Función combine *)
 let rec combine l1 l2 =
   match (l1, l2) with
     ([], []) -> []
   | (h1::t1, h2::t2) -> (h1,h2)::(combine t1 t2)
-  | ([], _::_) | (_::_, []) -> raise (Invalid_argument "combine");;
+  | ([], _::_) | (_::_, []) ->
+    raise (Invalid_argument "combine");; (* No recursiva terminal *)
 
 (* TERCERA PARTE *)
 
@@ -117,12 +152,15 @@ let rec init len f =
   if len < 0
     then raise (Invalid_argument ("init"))
   else 
-    let rec aux_init cnt n f =
+    let rec aux_init aux cnt n f =
       if cnt >= n
-        then []
+        then aux
       else
-        (f cnt)::aux_init (cnt+1) n f in
-    aux_init 0 len f;;
+        aux_init ((f cnt)::aux) (cnt+1) n f in
+      let rec rev aux = function
+          [] -> aux
+        | h::t -> rev (h::aux) t in
+      rev [] (aux_init [] 0 len f);;
 
 (* Función rev *)
 let rev l =
@@ -145,7 +183,7 @@ let rec concat = function
       match l1 with
         [] -> l2
       | h::t -> h::aux_concat t l2 in
-    aux_concat hd (concat tl);;
+    aux_concat hd (concat tl);; (* No recursiva terminal *)
     
 (* Función flatten *)
 let rec flatten = function
@@ -155,13 +193,13 @@ let rec flatten = function
       match l1 with
         [] -> l2
       | h::t -> h::aux_flatten t l2 in
-    aux_flatten hd (flatten tl);;
+    aux_flatten hd (flatten tl);; (* No recursiva terminal *)
 
 (* Función map *)
 let rec map f l =
   match l with
     [] -> []
-  | h::t -> (f h)::(map f t);;
+  | h::t -> (f h)::(map f t);; (* No recursiva terminal *)
 
 (* Función rev_map *)
 let rec rev_map f =
@@ -175,10 +213,11 @@ let rec map2 f l1 l2 =
   match (l1,l2) with
     ([],[]) -> []
   | (_::_, []) | ([], _::_) -> raise (Invalid_argument "map2")
-  | (h1::t1, h2::t2) -> (f h1 h2)::(map2 f t1 t2);;
+  | (h1::t1, h2::t2) -> (f h1 h2)::(map2 f t1 t2);; (* No recursiva terminal *)
 
 (* Función fold_left *)
-let rec fold_left f a = function
+let rec fold_left f a l =
+  match l with
     [] -> a
   | h::t -> fold_left f (f a h) t;;
 
@@ -186,4 +225,4 @@ let rec fold_left f a = function
 let rec fold_right f l a =
   match l with
     [] -> a
-  | h::t -> f h (fold_right f t a);;
+  | h::t -> f h (fold_right f t a);; (* No recursiva terminal *)
--- PRIMES/primes.ml
+++ PRIMES/primes.ml
@@ -1,26 +1,40 @@
+(* Título:  Primes			                      *)
+(* Author:  Óscar Alejandro Manteiga Seoane   *)
+(* Login:   oscar.manteiga@udc.es             *)
+(* Grupo:   4.5                               *)
+
 let is_prime n = 
     let rec not_div_from d = 
-	    d * d > n || 
-		(n mod d <> 0 && not_div_from (d+1))
-    in
+	    d * d > n || (n mod d <> 0 && not_div_from (d+1)) in
     n > 1 && not_div_from 2;;
-
 	
-let rec next_prime n = 
-    let n = succ n in
-	if is_prime n then n else next_prime n;;
+let rec next_prime n =
+	if n < 2
+		then 2
+	else
+		let n = (n+1) in 
+	let rec prm d = 
+		d*d > n || (n mod d <> 0 && prm (d+1)) in
+	if prm n
+		then n
+	else
+		next_prime (n+1);;
 
 	
 let rec prev_prime	n = (* n > 2 *)
 	let n = pred n in
-    if is_prime n then n else prev_prime n;;
+		if is_prime n
+			then n
+		else prev_prime n;;
 	
 	
 let rec primes_between m n = 
-	if m > n then [] else
-	if is_prime m then m :: primes_between (m+1) n
+	if m > n
+		then []
+	else
+		if is_prime m
+			then m :: primes_between (m+1) n
 	else primes_between (m+1) n;;
 	
 	
 let primes_till n = primes_between 2 n;;
-	
