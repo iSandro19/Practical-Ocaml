68992d0fed218e35dfdbb1e1ee1efddae5ad383d
iSandro19 <manteigaseoaneoscar@gmail.com>
2020-11-20 13:19:11 +0100

    Ejercicio opcional

--- P3/ej34.ml
+++ P3/ej34.ml
@@ -4,3 +4,15 @@
 (* Grupo:   4.5                               *)
 
 (* Ejercicio 4: Goldbach *)
+let goldbach n =
+  if (n mod 2) <> 0
+    then raise (Invalid_argument "goldbach")
+  else
+    let rec aux d =
+      if
+        let rec prm d =
+          d * d > n || (n mod d <> 0 && prm (d+1)) in
+        prm d && prm (n-d) 
+        then (d, n-d)
+      else aux (d+1)
+    in aux  2;;
